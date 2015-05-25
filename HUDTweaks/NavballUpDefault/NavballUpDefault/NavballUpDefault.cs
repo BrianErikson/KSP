cc51fac9a036b3d81cbb7fad07c3260a18e5209d
Brian Erikson <briankerikson@gmail.com>
2015-05-25 12:37:38 +0200

    Added NavballDefaultUp init

---
+++ HUDTweaks/NavballUpDefault/NavballUpDefault/NavballUpDefault.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace NavballUpDefault
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class NavballUpDefault : UnityEngine.MonoBehaviour
+    {
+        void Start()
+        {
+            MapView mapView = MapView.fetch;
+            if (!mapView.MapCollapse_navBall.expanded)
+            {
+                mapView.maneuverModeToggle.OnPress.Invoke();
+            }
+            mapView = null;
+        }
+
+        void Update()
+        {
+        }
+
+        void OnDestroy()
+        {
+
+        }
+    }
+}
